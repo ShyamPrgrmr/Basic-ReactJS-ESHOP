a66d81e6cd0a444bd2ffef3911caa2b1b6467238
ShyamPrgrmr <2018bcs509@sggs.ac.in>
2020-06-19 20:02:51 +0530

    Updates

---
+++ precache-manifest.ad203530f6c7a8754bc541a1a1a582b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae147343a56ad22a1b35bd7ee645a402",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "f43dc85cb449e16340db",
+    "url": "/myapp/static/css/main.fd3f6b07.chunk.css"
+  },
+  {
+    "revision": "6b462d4c36280473302e",
+    "url": "/myapp/static/js/2.9f312670.chunk.js"
+  },
+  {
+    "revision": "6ef718eeb39c03c0fcdab1a60b0c6b00",
+    "url": "/myapp/static/js/2.9f312670.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f43dc85cb449e16340db",
+    "url": "/myapp/static/js/main.2fc19d75.chunk.js"
+  },
+  {
+    "revision": "41c871ad5aea13463eb7",
+    "url": "/myapp/static/js/runtime-main.725d91b8.js"
+  }
+]);
